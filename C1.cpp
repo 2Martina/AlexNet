1b856e8aa1e6dfbd43d9d611cb5d1790cbd77d68
yasmiinezaki <71704853+yasmiinezaki@users.noreply.github.com>
2023-12-30 00:45:32 +0200

    Update C1.cpp

--- C1.cpp
+++ C1.cpp
@@ -20,18 +20,15 @@ const int NUM_FILTERS = 96;*/
 
 
 // Function to perform fixed-point 3D convolution
-void convolution3D(
-    const FixedPoint input[INPUT_CHANNELS][INPUT_SIZE][INPUT_SIZE],
-    const FixedPoint filters[NUM_FILTERS][INPUT_CHANNELS][FILTER_SIZE][FILTER_SIZE],
-    const FixedPoint biases[NUM_FILTERS],
-    int output_size
-) {
-    FixedPoint output[NUM_FILTERS][55][55]; // Assuming fixed output size, change accordingly
+void convolution3D( FixedPoint input[INPUT_CHANNELS][INPUT_SIZE][INPUT_SIZE],
+                    FixedPoint output[NUM_FILTERS][OUTPUT_SIZE][OUTPUT_SIZE],
+                    FixedPoint filters[NUM_FILTERS][INPUT_CHANNELS][FILTER_SIZE][FILTER_SIZE],
+                    FixedPoint biases[NUM_FILTERS]) {
 
     // Convolution implementation
     for (int f = 0; f < NUM_FILTERS; ++f) {
-        for (int i = 0; i < output_size; ++i) {
-            for (int j = 0; j < output_size; ++j) {
+        for (int i = 0; i < OUTPUT_SIZE; ++i) {
+            for (int j = 0; j < OUTPUT_SIZE; ++j) {
                 output[f][i][j] = 0;
                 for (int c = 0; c < INPUT_CHANNELS; ++c) {
                     for (int k = 0; k < FILTER_SIZE; ++k) {
@@ -50,9 +47,10 @@ void convolution3D(
 
 int main() {
     FixedPoint input[INPUT_CHANNELS][INPUT_SIZE][INPUT_SIZE];
+    FixedPoint output[NUM_FILTERS][OUTPUT_SIZE][OUTPUT_SIZE];
     FixedPoint filters[NUM_FILTERS][INPUT_CHANNELS][FILTER_SIZE][FILTER_SIZE];
     FixedPoint biases[NUM_FILTERS];
-    convolution3D(input, filters, biases, OUTPUT_SIZE);
+    convolution3D(input,output,filters, biases);
     return 0;
 }
 
