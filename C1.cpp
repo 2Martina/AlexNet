e7fecc8e069bdb5b228011351ad0e35f430dc50d
Martina Magued <96735308+2Martina@users.noreply.github.com>
2023-12-29 23:58:39 +0200

    Update C1.cpp

--- C1.cpp
+++ C1.cpp
@@ -1,50 +1,42 @@
-//_______Conv1_______
-
-
 #include <iostream>
-#include <vector>
+#include <ap_fixed.h>
 
-#include <ap_fixed.h> //class template from the Vivado-HLS library
-typedef ap_fixed<16,7,AP_RND> FixedPoint;  //total:16 , fractional:7 , rounding to the nearest representable value
+typedef ap_fixed<16, 7, AP_RND> FixedPoint;
 using namespace std;
 
-const int INPUT_SIZE = 227;
+/*const int INPUT_SIZE = 227;
 const int INPUT_CHANNELS = 3;
 const int FILTER_SIZE = 11;
 const int STRIDE = 4;
-const int NUM_FILTERS = 96;
+const int NUM_FILTERS = 96;*/
 
 
-
-//using FixedPoint = int16_t;  // 16 bits for integer part
-//const int SCALE_FACTOR = 16;  // 16 bits for fractional part
+#define INPUT_SIZE 227
+#define INPUT_CHANNELS 3
+#define FILTER_SIZE 11
+#define STRIDE 4
+#define NUM_FILTERS 96
+#define OUTPUT_SIZE 55
 
 
 // Function to perform fixed-point 3D convolution
-vector<vector<vector<FixedPoint>>> convolution3D(
-    const vector<vector<vector<FixedPoint>>>& input,
-    const vector<vector<vector<FixedPoint>>>& filters,
-    const vector<FixedPoint>& biases
+void convolution3D(
+    const FixedPoint input[INPUT_CHANNELS][INPUT_SIZE][INPUT_SIZE],
+    const FixedPoint filters[NUM_FILTERS][INPUT_CHANNELS][FILTER_SIZE][FILTER_SIZE],
+    const FixedPoint biases[NUM_FILTERS],
+    int output_size
 ) {
-    int input_size = input.size();
-    int filter_size = filters[0].size();
-    int output_size = (input_size - filter_size) / STRIDE + 1;
-
-    // Output initialization
-    vector<vector<vector<FixedPoint>>> output(
-        NUM_FILTERS,
-        vector<vector<FixedPoint>>(output_size, vector<FixedPoint>(output_size, 0))
-    );
+    FixedPoint output[NUM_FILTERS][55][55]; // Assuming fixed output size, change accordingly
 
     // Convolution implementation
     for (int f = 0; f < NUM_FILTERS; ++f) {
-        for (int i = 0; i < output_size; ++i) {    //output rows
-            for (int j = 0; j < output_size; ++j) {  //output columns
+        for (int i = 0; i < output_size; ++i) {  // Output rows
+            for (int j = 0; j < output_size; ++j) {  // Output columns
                 FixedPoint sum = 0;
 
                 for (int c = 0; c < INPUT_CHANNELS; ++c) {
-                    for (int fi = 0; fi < filter_size; ++fi) {    //rows
-                        for (int fj = 0; fj < filter_size; ++fj) {  //columns 
+                    for (int fi = 0; fi < FILTER_SIZE; ++fi) {  // Rows
+                        for (int fj = 0; fj < FILTER_SIZE; ++fj) {  // Columns
                             sum += input[c][i * STRIDE + fi][j * STRIDE + fj] *
                                    filters[f][c][fi][fj];
                         }
@@ -54,41 +46,46 @@ vector<vector<vector<FixedPoint>>> convolution3D(
                 // Add bias
                 sum += biases[f];
 
-                // Apply activation function (ReLU)
-                output[f][i][j] = (sum > 0) ? sum : 0;
             }
         }
     }
-
-    return output;
 }
 
 int main() {
     // Sample input, filters, and biases
-    vector<vector<vector<FixedPoint>>> input(INPUT_CHANNELS,
-                                             vector<vector<FixedPoint>>(INPUT_SIZE,
-                                                                      vector<FixedPoint>(INPUT_SIZE, 1)));
+    FixedPoint input[INPUT_CHANNELS][INPUT_SIZE][INPUT_SIZE] = {{{1}}};  // Initialize with your actual data
+    FixedPoint filters[NUM_FILTERS][INPUT_CHANNELS][FILTER_SIZE][FILTER_SIZE] = {{{{1}}}};  // Initialize with your actual data
+    FixedPoint biases[NUM_FILTERS] = {1};  // Initialize with your actual data
 
-    vector<vector<vector<FixedPoint>>> filters(NUM_FILTERS,
-                                               vector<vector<FixedPoint>>(INPUT_CHANNELS,
-                                                                        vector<FixedPoint>(FILTER_SIZE,
-                                                                                         vector<FixedPoint>(FILTER_SIZE, 1))));
 
-    vector<FixedPoint> biases(NUM_FILTERS, 1);  //default=1 //we should write all the biaes --------------------
+    FixedPoint output[NUM_FILTERS][OUTPUT_SIZE][OUTPUT_SIZE];
+
 
     // Convolution
-    vector<vector<vector<FixedPoint>>> result = convolution3D(input, filters, biases);
+    convolution3D(input, filters, biases, OUTPUT_SIZE);
+
+
+    for (int f = 0; f < NUM_FILTERS; ++f) {
+        for (int i = 0; i < OUTPUT_SIZE; ++i) {
+            for (int j = 0; j < OUTPUT_SIZE; ++j) {
+                int output_index = f * OUTPUT_SIZE * OUTPUT_SIZE + i * OUTPUT_SIZE + j;
+
+                // Apply ReLU activation
+                output[f][i][j] = (output[f][i][j] > 0) ? output[f][i][j] : FixedPoint(0);
+            }
+        }
+    }
 
     // Display result (for illustration)
-    for (int f = 0; f < NUM_FILTERS; ++f) {
-        for (int i = 0; i < result[0].size(); ++i) {
-            for (int j = 0; j < result[0][0].size(); ++j) {
-                cout << result[f][i][j] << " ";
+   /* for (int f = 0; f < NUM_FILTERS; ++f) {
+        for (int i = 0; i < output_size; ++i) {
+            for (int j = 0; j < output_size; ++j) {
+                cout << output[f][i][j] << " ";
             }
             cout << endl;
         }
         cout << endl;
-    }
+    }*/
 
     return 0;
 }
